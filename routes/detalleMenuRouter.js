13107baf843b571fbe573975aa10d4f440c0fc2a
Diribar <76135160+Diribar@users.noreply.github.com>
2021-02-12 14:57:55 -0300

    Todos los menús vinculados mediante rutinas

--- routes/detalleMenuRouter.js
+++ routes/detalleMenuRouter.js
@@ -1,10 +1,10 @@
 var express = require('express');
-
+var menu = require('../public/javascripts/menu')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('detalleMenuView');
+  res.render('detalleMenuView', {'menu':menu});
 });
 
 module.exports = router;
